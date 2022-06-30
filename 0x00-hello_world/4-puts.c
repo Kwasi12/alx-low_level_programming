291db6f24d54a80885d83997268f0649fb1bb16e
bismarkkay <irishmultimedia@gmail.com>
2022-06-30 12:39:49 -0400

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+/**
+ * main prints the main function
+ * the put function prints the strings through the output
+ */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle \n");
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
 }
 
