c63c48bc52410523bb8b48c942f7a08c0d1e8221
bismarkkay <irishmultimedia@gmail.com>
2022-07-05 15:20:07 -0400

    p

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,14 +6,13 @@
  * Return: 0
  */
 
-int print_last_digit(int n)
+int print_last_digit(int c)
 {
-	int last_digit = n % 10;
+	int last_digit = c % 10;
 
 	if (last_digit < 0)
 		last_digit *= -1;
 
 	_putchar(last_digit + '0');
-
 	return (last_digit);
 }
