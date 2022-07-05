bd1013363a25b0cffea7c58148f262a0977e168e
bismarkkay <irishmultimedia@gmail.com>
2022-07-05 15:17:52 -0400

    p

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,6 +2,7 @@
 /**
  * print_last_digit - function that computes the absolute value of an integer
  * @c:  is the int that will use for the argument of the function
+ * @n: The number in question.
  * Return: 0
  */
 
