db22513435a94be88504f71c7b801d335058ca9c
Kwasi12 <106727078+Kwasi12@users.noreply.github.com>
2022-09-10 00:26:34 +0000

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
-#include <unistd.h>
+
 /**
- * main -Entry point
- * Description: prints allsingle digit numbers of
- * base 10 from 0
- * Return: Always 0 (success)
+ *main -> assign a random number to the variable n each time it is executed
+ *and print the last digit of the number stored in the variable n
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
-	int a;
+int ch;
+for (ch = 48; ch <= 57; ch++)
+{
 
-	for (a = 0; a < 10; a++)
-	{
-		printf("%d", a);
-	}
-	putchar('\n');
-	return (0);
+putchar(ch);
+}
+
+putchar(10); /* this is an ascii code for new line*/
+
+return (0);
 }
